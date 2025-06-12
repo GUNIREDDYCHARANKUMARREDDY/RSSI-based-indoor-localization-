4703431391cb2e0b33a3c7915a64f00f28f28554
GUNIREDDYCHARANKUMARREDDY <charanreddy2908@gmail.com>
2025-06-12 13:52:20 +0530

    Delete Arduino-RSSI-Positioning directory

--- Arduino-RSSI-Positioning/ACCESS_POINT_AP1/ACCESS_POINT_AP1.ino
+++
@@ -1,9 +0,0 @@
-#include <WiFi.h>
-
-void setup() {
-  WiFi.softAP("AP1");  // Unique SSID
-  Serial.begin(115200);
-  Serial.println("AP1 started");
-}
-
-void loop() {}
--- Arduino-RSSI-Positioning/ACCESS_POINT_AP2/ACCESS_POINT_AP2.ino
+++
@@ -1,9 +0,0 @@
-#include <WiFi.h>
-
-void setup() {
-  WiFi.softAP("AP2");  // Unique SSID
-  Serial.begin(115200);
-  Serial.println("AP2 started");
-}
-
-void loop() {}
